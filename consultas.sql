9b46ce7b375d35770a0492e7560be81dd96c423e
Carlos Alvarado Lima <164815713+Carlos-Lima2510@users.noreply.github.com>
2025-01-11 20:56:34 +0100

    Create consultas.sql

---
+++ consultas.sql
@@ -0,0 +1,64 @@
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS `consultas_alejandro` //
+
+CREATE DEFINER=`root`@`%` PROCEDURE `consultas_alejandro`()
+BEGIN
+
+-- Consultas --
+
+END //
+
+DELIMITER;
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS `consultas_kelvia` //
+
+CREATE DEFINER=`root`@`%` PROCEDURE `consultas_kelvia`()
+BEGIN
+
+-- Consultas --
+
+END //
+
+DELIMITER;
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS `consultas_carlos` //
+
+CREATE DEFINER=`root`@`%` PROCEDURE `consultas_carlos`()
+BEGIN
+
+-- Consultas --
+
+END //
+
+DELIMITER;
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS `consultas_manuel` //
+
+CREATE DEFINER=`root`@`%` PROCEDURE `consultas_manuel`()
+BEGIN
+
+-- Consultas --
+
+END //
+
+DELIMITER;
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS `consultas_miguel` //
+
+CREATE DEFINER=`root`@`%` PROCEDURE `consultas_miguel`()
+BEGIN
+
+-- Consultas --
+
+END //
+
+DELIMITER;
