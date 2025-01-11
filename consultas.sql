afca79686b26bd795cd979f0c845d21a22aa2496
Carlos Alvarado Lima <164815713+Carlos-Lima2510@users.noreply.github.com>
2025-01-11 20:58:40 +0100

    Update consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -2,10 +2,20 @@ DELIMITER //
 
 DROP PROCEDURE IF EXISTS `consultas_alejandro` //
 
-CREATE DEFINER=`root`@`%` PROCEDURE `consultas_alejandro`()
+CREATE DEFINER=`carlos.alvarado`@`%` PROCEDURE `consultas_alejandro`()
 BEGIN
 
--- Consultas --
+--Interseccion--
+SELECT nombre, correo_electronico FROM Usuarios WHERE nombre IN ('Alejandro Juárez', 'Carlos Lima', 'Miguel Rivas') INTERSECT SELECT nombre, correo_electronico FROM Usuarios WHERE nombre IN ('Carlos Lima', 'Kelvia Neves', 'Miguel Rivas');
+
+--Union--
+SELECT nombre, correo_electronico FROM Usuarios WHERE nombre IN ('Carlos Lima', 'Miguel Rivas') UNION SELECT nombre, correo_electronico FROM Usuarios WHERE nombre IN ('Alejandro Juárez', 'Kelvia Neves');
+
+--Diferencia--
+SELECT nombre, correo_electronico FROM Usuarios WHERE nombre IN ('Carlos Lima', 'Miguel Rivas', 'Alejandro Juárez') EXCEPT SELECT nombre, correo_electronico FROM Usuarios WHERE nombre IN ('Miguel Rivas', 'Kelvia Neves');
+
+--Agregación--
+SELECT COUNT(*) AS total_usuarios FROM Usuarios;
 
 END //
 
@@ -15,7 +25,7 @@ DELIMITER //
 
 DROP PROCEDURE IF EXISTS `consultas_kelvia` //
 
-CREATE DEFINER=`root`@`%` PROCEDURE `consultas_kelvia`()
+CREATE DEFINER=`carlos.alvarado`@`%` PROCEDURE `consultas_kelvia`()
 BEGIN
 
 -- Consultas --
@@ -28,10 +38,24 @@ DELIMITER //
 
 DROP PROCEDURE IF EXISTS `consultas_carlos` //
 
-CREATE DEFINER=`root`@`%` PROCEDURE `consultas_carlos`()
+CREATE DEFINER=`carlos.alvarado`@`%` PROCEDURE `consultas_carlos`()
 BEGIN
 
--- Consultas --
+-- Intersección
+
+SELECT * FROM prestamo WHERE duracion = 4 INTERSECT SELECT * FROM prestamo WHERE fecha_registro > '2025-01-01 00:00:00:';
+
+-- Diferencia
+
+SELECT * FROM prestamo WHERE duracion = 6 EXCEPT SELECT * FROM prestamo WHERE fecha_registro < '2025-01-01 00:00:00:';
+
+-- Agregación de duración promedio
+
+SELECT AVG(duracion) FROM prestamo;
+
+-- Union
+
+SELECT * FROM prestamo WHERE duracion = 4 UNION SELECT * FROM prestamo WHERE fecha_registro > '2025-01-01 00:00:00:';
 
 END //
 
@@ -41,7 +65,7 @@ DELIMITER //
 
 DROP PROCEDURE IF EXISTS `consultas_manuel` //
 
-CREATE DEFINER=`root`@`%` PROCEDURE `consultas_manuel`()
+CREATE DEFINER=`carlos.alvarado`@`%` PROCEDURE `consultas_manuel`()
 BEGIN
 
 -- Consultas --
@@ -54,7 +78,7 @@ DELIMITER //
 
 DROP PROCEDURE IF EXISTS `consultas_miguel` //
 
-CREATE DEFINER=`root`@`%` PROCEDURE `consultas_miguel`()
+CREATE DEFINER=`carlos.alvarado`@`%` PROCEDURE `consultas_miguel`()
 BEGIN
 
 -- Consultas --
