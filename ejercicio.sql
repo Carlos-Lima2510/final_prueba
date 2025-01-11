8d255896f783ddca295b0f3e94027a0b3955e87a
Carlos-Lima2510 <carlos251005.alvarado@gmail.com>
2025-01-11 19:40:27 +0100

    tablas carlos

--- ejercicio.sql
+++ ejercicio.sql
@@ -1,1 +1,11 @@
 -- Aqui van las tablas de cada uno -- 
+
+CREATE TABLE IF NOT EXISTS prestamo (
+    id_prestamo int PRIMARY KEY AUTO_INCREMENT,
+    fecha_registro timestamp DEFAULT CURRENT_TIMESTAMP not null,
+    duracion int not null,
+    fecha_fin datetime not null
+) ENGINE=MyISAM CHARSET=utf8 COLLATE=utf8mb3_general_ci;
+
+INSERT INTO prestamo (duracion, fecha_fin)
+VALUES (4, "2025-01-16 00:00:00");
